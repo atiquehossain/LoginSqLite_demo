93abcc5df6b8d47eed8f7470264cc608c04ea1c2
Atique <atique.hossain@savethechildren.org>
2021-08-23 21:13:31 +0600

    LoginUI done

--- app/src/main/java/com/example/loginSQLiteDemo/MainActivity.java
+++ app/src/main/java/com/example/loginSQLiteDemo/MainActivity.java
@@ -2,13 +2,62 @@ package com.example.loginSQLiteDemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import pl.bclogic.pulsator4droid.library.PulsatorLayout;
 
 public class MainActivity extends AppCompatActivity {
+    EditText email,password;
+    CheckBox cbox;
+    String emailtext,passwordtext;
+    Button button;
+    boolean chcckboxvalue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
+        button=(Button)findViewById(R.id.button1) ;
+
+
+        PulsatorLayout mPulsatorAnimation = (PulsatorLayout) findViewById(R.id.pro_pic_animation);
+        mPulsatorAnimation.start();
+
+
+        
+
+    }
+    private boolean validation() {
+        boolean flag = true;
+        if (email.getText().toString().isEmpty()) {
+            Toast.makeText(MainActivity.this, "You must enter email  to login", Toast.LENGTH_SHORT).show();
+            flag = false;
+        }  else if (password.getText().toString().isEmpty()) {
+            Toast.makeText(MainActivity.this, "You must enter password  to login!", Toast.LENGTH_SHORT).show();
+            flag = false;
+        }
+
+        return flag;
+    }
+
+
+
+
+    public void btn(View view) {
+       if(validation()) {
+
+       }
+
+
     }
 }
